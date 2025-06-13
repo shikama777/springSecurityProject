cc846a5b04c88ceb76a381807dc6718c8c6274e7
shikama777 <shikamayuya777@gmail.com>
2025-06-13 21:27:20 +0900

    Google認証

--- springSecurityProject/src/main/java/myapp/config/SecurityConfig.java
+++ springSecurityProject/src/main/java/myapp/config/SecurityConfig.java
@@ -5,28 +5,28 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-	@Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	/*@Bean
+	public PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
+	}*/
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(authorize -> authorize
+            	.requestMatchers( "/login").permitAll()            		
                 .anyRequest().authenticated()
             )
-            .formLogin(formLogin -> formLogin
-                .permitAll()
-            )
+            .oauth2Login(oauth -> oauth
+                    .loginPage("/login")  // 自作のログインページ
+                    .defaultSuccessUrl("/")  // ログイン成功後のリダイレクト先
+                )
             .rememberMe(Customizer.withDefaults());
 
         return http.build();
